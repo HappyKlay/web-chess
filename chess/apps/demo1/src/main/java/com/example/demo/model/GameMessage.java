8c0793f031cc5c61cf5b7954b54b257baab92e1a
ENVY <91910187+HappyKlay@users.noreply.github.com>
2025-07-20 23:31:47 +0300

    Update GameMessage.java

--- chess/apps/demo1/src/main/java/com/example/demo/model/GameMessage.java
+++ chess/apps/demo1/src/main/java/com/example/demo/model/GameMessage.java
@@ -14,15 +14,15 @@ public class GameMessage {
     private String content;
     
     public enum MessageType {
-        JOIN,           // User joining a game
-        START,          // Game starting
-        LEAVE,          // User leaving a game
-        NAME_EXCHANGE,  // User name exchange
-        MOVE,           // Game move
-        PLAYER_INFO,    // Get Player's info
-        GAME_OVER,      // Game over (checkmate, draw, etc.)
-        DRAW_OFFER,     // Player offering a draw
-        DRAW_RESPONSE,   // Response to a draw offer (accept/decline)
+        JOIN,          
+        START,          
+        LEAVE,         
+        NAME_EXCHANGE,  
+        MOVE,         
+        PLAYER_INFO,    
+        GAME_OVER,     
+        DRAW_OFFER,    
+        DRAW_RESPONSE,  
         TIMER_UPDATE
     }
 }
